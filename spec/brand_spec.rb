53e8dba6bbd4a9a9d2680ab71c64866eefc64724
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-21 12:18:11 +0300

    added a spec for finding the shoes' brands using their id.

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,10 +1,21 @@
-require("spec_helper")
+require('spec_helper')
 
 describe(Brand) do
+  # starts with no shoe-brands.
+  describe('.all') do
+    it('starts off with no brands') do
+      expect(Brand.all).to(eq([]))
+    end
+  end
 
-  describe(".all") do
-    it("starts off with no brands") do
-      expect(Brand.all()).to(eq([]))
+  #finds the shoe-brands using their ids.
+  describe(".find") do
+    it("returns a brand by its ID number") do
+      test_brand = Brand.new({:name => "Akala", :id => nil})
+      test_brand.save()
+      test_brand2 = Brand.new({:name => "Rubber", :id => nil})
+      test_brand2.save()
+      expect(Brand.find(test_brand2.id())).to(eq(test_brand2))
     end
   end
 end
