1d8bf8bb31be7bf58caf6936dd89b9634dab691a
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-22 15:20:29 +0300

    added the activerecord.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,8 @@
+ENV['RAKE_ENV'] = 'test'
 require("rspec")
   require("pg")
   require("store")
   require('brand')
-
-  DB = PG.connect({:dbname => "shoe_database_test"})
 
   RSpec.configure do |config|
     config.after(:each) do
