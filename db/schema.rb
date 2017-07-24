c133530008b38594ee2a7f7c58dd92eb4ccaaf15
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-24 09:58:19 +0300

    added a table for shoe_brands

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 20170724063038) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
