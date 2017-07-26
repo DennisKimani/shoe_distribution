54f96ff6f8e4dc90e976af206787423780f44669
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-26 08:44:39 +0300

    added new columns in the table.

--- db/migrate/20170725114342_brands_stores.rb
+++ db/migrate/20170725114342_brands_stores.rb
@@ -3,6 +3,8 @@ class BrandsStores < ActiveRecord::Migration[5.1]
     create_table(:brands_stores) do |t|
       t.column(:store_id, :integer)
       t.column(:brand_id, :integer)
+      t.column(:store_name, :string)
+      t.column(:brand_name, :string)
 
       t.timestamps()
     end
